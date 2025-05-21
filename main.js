0793454376d736645ffe4275131a7d4b42a464b2
John A. Jacobs <john.a.jacobs1973@gmail.com>
2025-05-21 19:44:45 -0400

    Update h1

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const btn = document.querySelector('input')
+const text = document.querySelector('h1')
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    btn.classList.toggle('spin')
+    text.classList.toggle('spinl')
+})
